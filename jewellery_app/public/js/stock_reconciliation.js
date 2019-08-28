de9157ac2b7b270ff1a7f0a4866ef401c4112ccc
ashish-greycube <mr.ashish.shah@gmail.com>
2019-08-28 14:59:21 +0530

    get items by item group

--- jewellery_app/public/js/stock_reconciliation.js
+++ jewellery_app/public/js/stock_reconciliation.js
@@ -3,29 +3,26 @@ frappe.provide("erpnext.stock");
 frappe.ui.form.on("Stock Reconciliation", {
     refresh: function(frm) {
 		if(frm.doc.docstatus < 1) {
-			frm.add_custom_button(__("Items Group"), function() {
+			frm.add_custom_button(__("Jewellery Items Group"), function() {
 				frm.events.get_jewllery_items(frm);
 			});
 		}
     },
     get_jewllery_items: function(frm) {
 
-		frappe.prompt({label:"Item Group", fieldname: "item_group", fieldtype:"Link", options:"Item Group", reqd: 1,
-			"get_query": function() {
-				return {
-					"filters": {
-						"company": frm.doc.company,
-					}
-				}
-			}},
+		frappe.prompt([
+			{label:"Item Group", fieldname: "item_group", fieldtype:"Link", options:"Item Group", reqd: 1},
+			{label:"New Valuation Rate", fieldname: "jewel_valuation_rate", fieldtype:"Currency", reqd: 1}		
+		],
 			function(data) {
 				frappe.call({
-					method:"erpnext.stock.doctype.stock_reconciliation.stock_reconciliation.get_items",
+					method:"jewellery_app.api.stock_reconciliation_get_items",
 					args: {
-						warehouse: data.warehouse,
+						warehouse: 'All Warehouses - '+frappe.get_abbr(frm.doc.company),
 						posting_date: frm.doc.posting_date,
 						posting_time: frm.doc.posting_time,
-						company:frm.doc.company
+						company:frm.doc.company,
+						item_group:data.item_group
 					},
 					callback: function(r) {
 						var items = [];
@@ -34,12 +31,13 @@ frappe.ui.form.on("Stock Reconciliation", {
 							var d = frm.add_child("items");
 							$.extend(d, r.message[i]);
 							if(!d.qty) d.qty = null;
-							if(!d.valuation_rate) d.valuation_rate = null;
+							d.valuation_rate =data.jewel_valuation_rate
+							// if(!d.valuation_rate) d.valuation_rate = null;
 						}
 						frm.refresh_field("items");
 					}
 				});
 			}
-		, __("Get Items"), __("Update"));
+		, __("Get Items by Items Group"), __("Update"));
 	}
 });
